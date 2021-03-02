8b57afe48c43c19b69bacf5f69941e50e4b15783
Michael <mholmes2@student.unimelb.edu.au>
2021-03-03 00:33:08 +1100

    add first of header blocks

--- mLas4.js
+++ mLas4.js
@@ -1,4 +1,4 @@
-function stringFromUInt8(A) {
+function toString(A) {
     let count = A.length;
     let str = "";
 
@@ -8,7 +8,46 @@ function stringFromUInt8(A) {
     return str;
 }
 
+const Block = (name,type,size, func) => {
+    return {
+        name: name,
+        type: type,
+        size: size,
+        func: func
+    }
+}
+// char - uint8
+// ushort - uint16
+// ulong - uint32
+
+var DEF_LAS1_4 = [
+    Block("fileSignature", Uint8Array, 4, toString),
+    Block("fileSourceId", Uint16Array, 2, null),
+    Block("globalEncoding", Uint16Array, 2, null),
+    Block("projectGuidId1", Uint32Array, 4, null),
+    Block("projectGuidId2", Uint8Array, 2, null),
+    Block("projectGuidId3", Uint8Array, 2, null),
+    Block("projectGuidId4", Uint8Array, 8, null),
+    Block("versionMajor", Uint8Array, 1, null),
+    Block("versionMinor", Uint8Array, 1, null),
+    Block("systemIdentifier", Uint8Array, 32, toString),
+    Block("generatingSoftware", Uint8Array, 32, toString),
+    Block("fileCreationDayOfYear", Uint16Array, 2, null),
+    Block("fileCreationYear", Uint16Array, 2, null),
+    Block("headerSize", Uint16Array, 2, null),
+    Block("offsetToPointData", Uint32Array, 4, null),
+    Block("numberOfVariableLengthRecords", Uint32Array, 4, null),
+    Block("pointDataRecordFormat", Uint8Array, 1, null),
+    Block("pointDataRecordLength", Uint16Array, 2, null),
+    Block("legacyNumberofPointRecords", Uint32Array, 4, null),
+    Block("legacyNumberofPointsByReturn", Uint32Array, 20, null),
+
+];
+console.log(DEF_LAS1_4);
+
+
 function readlas4(evt) {
+    let _idx = 0;
     var _header = {
         fileSignature:""
     }
@@ -20,10 +59,21 @@ function readlas4(evt) {
             let buffer = r.result;  
             console.log("Parsing Header...");
             try {
-                var headerBuff = buffer.slice(0,4);
-                var headerView = new Uint8Array(headerBuff);
-                _header.fileSignature = stringFromUInt8(headerView);
-                console.log("File Signature: " + _header.fileSignature);
+                for (let i = 0; i < DEF_LAS1_4.length; i++) {
+                    var headerBuff = buffer.slice(_idx, _idx + DEF_LAS1_4[i].size);
+                    _idx += DEF_LAS1_4[i].size;
+                    var headerView = DEF_LAS1_4[i].func 
+                                   ? DEF_LAS1_4[i].func(new DEF_LAS1_4[i].type(headerBuff)) 
+                                   : new DEF_LAS1_4[i].type(headerBuff);
+                    console.log(`${DEF_LAS1_4[i].name}: ` + headerView );
+                }
+
+                // var headerBuff = buffer.slice(0,4);
+                // var headerView = new Uint8Array(headerBuff);
+                // _header.fileSignature = toString(headerView);
+                // console.log("File Signature: " + _header.fileSignature);
+
+
             }
             catch (e) {
                 console.error(e);
